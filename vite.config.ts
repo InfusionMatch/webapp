2599134ad0415bbd729c7409567edb941c1bc494
InfusionMatch <jpregulman@gmail.com>
2025-10-06 13:41:36 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+     base: '/', // ensures all assets are loaded relative to root
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
