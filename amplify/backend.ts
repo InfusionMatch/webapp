d0f57a390a95b4720126056af12ee17a7ccfbe3b
InfusionMatch <jpregulman@gmail.com>
2025-10-09 04:16:19 +0000

    Simplify backend to basic configuration

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,36 +3,8 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   storage
-});
-
-// HIPAA: Enable encryption and versioning for S3
-const s3Bucket = backend.storage.resources.bucket;
-
-// Enable server-side encryption
-s3Bucket.addPropertyOverride('BucketEncryption', {
-  ServerSideEncryptionConfiguration: [
-    {
-      ServerSideEncryptionByDefault: {
-        SSEAlgorithm: 'AES256'
-      },
-      BucketKeyEnabled: true
-    }
-  ]
-});
-
-// Enable versioning for audit trail
-s3Bucket.addPropertyOverride('VersioningConfiguration', {
-  Status: 'Enabled'
-});
-
-// Block public access
-s3Bucket.addPropertyOverride('PublicAccessBlockConfiguration', {
-  BlockPublicAcls: true,
-  BlockPublicPolicy: true,
-  IgnorePublicAcls: true,
-  RestrictPublicBuckets: true
 });
